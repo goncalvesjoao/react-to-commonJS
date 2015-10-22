4e44ef07ea461933925a37715a8edfdfff7028d4
goncalvesjoao <goncalves.joao@gmail.com>
2015-10-22 18:41:58 +0100

    gulpfile was not setting the babel's state 1

--- boilerplates/css-modules/gulpfile.js
+++ boilerplates/css-modules/gulpfile.js
@@ -80,12 +80,12 @@ function webpackBasicConfig() {
         {
           test: /\.jsx$/,
           exclude: /node_modules/,
-          loaders: ['babel-loader']
+          loaders: ['babel?stage=1']
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loaders: ['babel-loader']
+          loaders: ['babel?stage=1']
         }
       ]
     },
